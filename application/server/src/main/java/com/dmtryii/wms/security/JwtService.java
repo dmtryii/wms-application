29d428bd2084dd4249f9787b42e28b4053850c48
dmtryii <dmitriy04tr@gmail.com>
2023-06-11 15:06:28 +0300

    SecurityConfig and JwtService refactoring

--- application/server/src/main/java/com/dmtryii/wms/security/JwtService.java
+++ application/server/src/main/java/com/dmtryii/wms/security/JwtService.java
@@ -5,6 +5,8 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +19,12 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "";
+    private final String SECRET_KEY;
+
+    @Autowired
+    public JwtService(@Value("${spring.security.secret.key}") String SECRET_KEY) {
+        this.SECRET_KEY = SECRET_KEY;
+    }
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
