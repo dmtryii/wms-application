74899bbac233f885ae7e1e502182cbaac2c7c876
dmtryii <dmitriy04tr@gmail.com>
2023-06-25 21:56:36 +0300

    added item entity

---
+++ application/server/src/main/java/com/dmtryii/wms/model/Item.java
@@ -0,0 +1,32 @@
+package com.dmtryii.wms.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Double price;
+    @Column(columnDefinition = "text")
+    private String description;
+    @OneToMany(mappedBy = "item", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Set<Stock> stock;
+    @OneToMany(mappedBy = "item", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Set<Supply> supply;
+}
